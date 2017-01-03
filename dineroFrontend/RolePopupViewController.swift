b26340c460e4f4bb451d24866978440d3f095053
WhiteRabbit27 <RKhillah@mail.ccsf.edu>
2017-01-03 14:58:11 -0500

    View Controllers added and assigned to Views
    LoginView hooked up to LoginViewController
    RegisterView hooked up to RegisterViewController
    RolePopupView hooked up to RolePopupViewController

---
+++ dineroFrontend/RolePopupViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RolePopupViewController.swift
+//  dineroFrontend
+//
+//  Created by Richard Khillah on 1/3/17.
+//  Copyright © 2017 RK Inc. All rights reserved.
+//
+
+import UIKit
+
+class RolePopupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
