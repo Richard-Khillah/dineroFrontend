2d954d905ede55adbea49748b2625b4c80dd3b60
WhiteRabbit27 <RKhillah@mail.ccsf.edu>
2017-01-03 15:06:11 -0500

    User text fields hooked up and cancel buttons implemented

--- dineroFrontend/RegisterViewController.swift
+++ dineroFrontend/RegisterViewController.swift
@@ -10,26 +10,23 @@ import UIKit
 
 class RegisterViewController: UIViewController {
 
+    @IBOutlet weak var userEmailTextField: UITextField!
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var userUsernameTextField: UITextField!
+    @IBOutlet weak var userPasswordTextField: UITextField!
+    @IBOutlet weak var userRetypePasswordTextField: UITextField!
+    @IBOutlet weak var userRoleTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func registerButtonTapped(_ sender: Any) {
     }
-    */
-
+    
+    @IBAction func cancelButtonTapped(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
 }
